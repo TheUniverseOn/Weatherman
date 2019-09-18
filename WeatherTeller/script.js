f253968b5a833fd5c5a7fd48a0f2152cf36b2bb7
Sophonias Isaac <sophonias8@gmail.com>
2019-09-18 12:09:16 +0100

    Update script.js

--- WeatherTeller/script.js
+++ WeatherTeller/script.js
@@ -1,22 +1,83 @@
-let appID ="818e46a1924f34d350cd9e41a102c700"; 
+let appId ="818e46a1924f34d350cd9e41a102c700"; 
 let units = 'metric'; 
-let searchMethod ='zip'; 
+let searchMethod; 
 
-function searchWeather(searchTerm){
-    fetch(`http://api.openweathermap.org/data/2.5/weather?${searchMethod}=${searchTerm}&APPID=${appID}&units=${units}`).then(result => {
-        return result.json(); 
-    }).then(result => {
-        init(result);
-    })
+function getSearchMethod(searchTerm) {
+    if(searchTerm.length === 5 && Number.parseInt(searchTerm) + '' === searchTerm)
+        searchMethod = 'zip';
+    else 
+        searchMethod = 'q';
 }
 
-
-function init(resultFromServer){
-console.log(resultFromServer);
+function searchWeather(searchTerm) {
+    getSearchMethod(searchTerm);
+    fetch(`http://api.openweathermap.org/data/2.5/weather?${searchMethod}=${searchTerm}&APPID=${appId}&units=${units}`)
+        .then((result) => {
+            return result.json();
+        }).then((res) => {
+            init(res);
+    });
 }
 
-document.getElementById('searchBtn').addEventListener('click', () =>{
-    let searchTerm = document.getElementById('searchInput').value
+function init(resultFromServer) {
+    switch (resultFromServer.weather[0].main) {
+        case 'Clear':
+            document.body.style.backgroundImage = "url('clear.jpg')";
+            break;
+        
+        case 'Clouds':
+            document.body.style.backgroundImage = "url('cloudy.jpg')";
+            break;
+
+        case 'Rain':
+        case 'Drizzle':
+        case 'Mist':
+            document.body.style.backgroundImage = "url('rain.jpg')";
+            break;
+        
+        case 'Thunderstorm':
+            document.body.style.backgroundImage = "url('storm.jpg')";
+            break;
+        
+        case 'Snow':
+            document.body.style.backgroundImage = "url('snow.jpg')";
+            break;
+
+        default:
+            break;
+    }
+
+    let weatherDescriptionHeader = document.getElementById('weatherDescriptionHeader');
+    let temperatureElement = document.getElementById('temperature');
+    let humidityElement = document.getElementById('humidity');
+    let windSpeedElement = document.getElementById('windSpeed');
+    let cityHeader = document.getElementById('cityHeader');
+
+    let weatherIcon = document.getElementById('documentIconImg');
+    weatherIcon.src = 'http://openweathermap.org/img/w/' + resultFromServer.weather[0].icon + '.png';
+
+    let resultDescription = resultFromServer.weather[0].description;
+    weatherDescriptionHeader.innerText = resultDescription.charAt(0).toUpperCase() + resultDescription.slice(1);
+    temperatureElement.innerHTML = Math.floor(resultFromServer.main.temp) + '&#176;';
+    windSpeedElement.innerHTML = 'Winds at  ' + Math.floor(resultFromServer.wind.speed) + ' m/s';
+    cityHeader.innerHTML = resultFromServer.name;
+    humidityElement.innerHTML = 'Humidity levels at ' + resultFromServer.main.humidity +  '%';
+
+    setPositionForWeatherInfo();
+}
+
+function setPositionForWeatherInfo() {
+    let weatherContainer = document.getElementById('weatherContainer');
+    let weatherContainerHeight = weatherContainer.clientHeight;
+    let weatherContainerWidth = weatherContainer.clientWidth;
+
+    weatherContainer.style.left = `calc(50% - ${weatherContainerWidth/2}px)`;
+    weatherContainer.style.top = `calc(50% - ${weatherContainerHeight/1.3}px)`;
+    weatherContainer.style.visibility = 'visible';
+}
+
+document.getElementById('searchBtn').addEventListener('click', () => {
+    let searchTerm = document.getElementById('searchInput').value;
     if(searchTerm)
-    searchWeather(searchTerm) 
-})
+        searchWeather(searchTerm);
+});
